ae34e6848bea746171cc50efa27e147552be7793
flucloxin500 <mdwalid826@gmail.com>
2023-09-27 00:31:29 +0600

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
+/*class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,102 +11,392 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatefulWidget{
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  /// for listing student
+  List<String> students = [
+    'WALID',
+    'ARSHAD',
+    'JEETU',
+    'AHSAASY',
+    'DIPTO',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        backgroundColor: Colors.teal,
-        leading: Icon(
-          Icons.account_balance_wallet_outlined,
-          color: Colors.lime,
-          size: 27,),
-        centerTitle: true,
-        title: Text('Homie',style: TextStyle(
-          color: Colors.black54,
-        ),),
+        title: Text('Home'),
       ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text('Hello '),
-              Text('World.! '),
-            ],
+      body: Scrollbar(
+        thickness: 30,
+        radius: Radius.circular(10),
+        /// Listview separator
+        /// Listview builder
+        //
+        child: ListView.separated(
+          itemCount: students.length,
+          itemBuilder: (context,index){
+
+            return ListTile(
+              onTap: (){
+                print('Touched builder');
+              },
+              title: Text(students[index])
+            );
+            },
+          separatorBuilder: (context,index){
+            return Divider();
+          },
+        ),
+        //child: ListView.builder(
+          // Listview.separated
+
+          //separatorBuilder: (context,index){
+          //   return Column(
+          //     children: [
+          //       Text(index.toString()),
+          //       Divider(),
+          //     ],
+          //   );
+            //return Divider();
+          //},
+          //itemCount: 18,
+            //itemBuilder: (context,index) {
+          //return ListTile(
+            //title: Text('Item number ${index}'),
+          //);
+        //},
+        //),
+        /*GridView.builder(
+          itemCount: 16,
+         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+           crossAxisCount: 2 ,
+         ),
+          itemBuilder: (context,index){
+           return Column(
+             children: [
+               Text(index.toString()),
+               Icon(Icons.adb_rounded),
+             ],
+           );
+          },
+
+        ),*/
+
+        ),
+      );
+
+  }
+}
+
+ */
+/// Navigator ==> one page to another page
+/// Navigator 1(simnple and lengthy),2(complex)
+/// Router package ==>GoROuter,GetX,AutoRoute
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.green,
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                elevation: 5,
+                textStyle: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.6,
+                  wordSpacing: 0.5,
+                ))),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            textStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+            foregroundColor: Colors.deepOrange,
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Dear Practice')
-            ],
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.purple,
+          elevation: 5,
+          shadowColor: Colors.green,
+        ),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(
+            fontSize: 18,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text('Hello '),
-              Text('Class '),
-            ],
-          ),
-          Image.asset('images/test.jpg',width: 120,height: 90,fit: BoxFit.cover,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Image.network('https://www.bing.com/images/search?view=detailV2&ccid=1YM53mG1&id=BC6470C60B7A8615DA7155B539C4A122275DF649&thid=OIP.1YM53mG10H_U25iPjop83QHaEo&mediaurl=https%3a%2f%2fwww.thewowstyle.com%2fwp-content%2fuploads%2f2015%2f01%2fnature-images..jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.d58339de61b5d07fd4db988f8e8a7cdd%3frik%3dSfZdJyKhxDm1VQ%26pid%3dImgRaw%26r%3d0&exph=1800&expw=2880&q=images&simid=608024970036858528&FORM=IRPRST&ck=D0378EAF346D6D862EE51853DCE82E23&selectedIndex=0&idpp=overlayview&ajaxhist=0&ajaxserp=0',
-                width: 200,
-                height: 200,
-                fit: BoxFit.scaleDown,
+          bodySmall: TextStyle(fontSize: 12),
+          bodyLarge: TextStyle(fontSize: 22),
+        ),
+      ),
+      darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.amberAccent,
+            elevation: 5,
+            shadowColor: Color(0xFFF082FF),
+          )
+      ),
+      themeMode: ThemeMode.dark,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello World!'),
+            Text(
+              'Hello World!',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            Text('Hello World!'),
+            TextButton(
+              onPressed: () {},
+              child: Text('Tap here'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Tap here'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                onPressed: () {},
+                child: Text('Tap here'),
               ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('To register, tap on '),
-              Text('Sign Up'),
-            ],
-          ),
-          RichText(text:
-          TextSpan(
-            style: TextStyle(fontSize: 20,color: Colors.lime),
-            children: [
-              TextSpan(
-                  text: 'Hello World!', style: TextStyle(color: Colors.cyanAccent)
-              ),
-              TextSpan(
-                  text: 'Namee', style: TextStyle(color: Colors.deepOrange)),
-              TextSpan(
-                  text: 'gfhg',
-                  style: TextStyle(
-                      fontSize: 23,
-                      color: Colors.blueGrey,
-                      fontWeight: FontWeight.w200)),
-            ],
-          ),
-          ),
-          RichText(text: TextSpan(
-              text: 'To register, tap on ',
-              children: [
-                TextSpan(
-                    text: 'Sign up',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18
-                    )
-                )
-              ]
-          ))
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
-
 }
-void main (){
-  runApp(MyApp());
 
+/// Navigator from one route to another
+
+/*class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Home',
+              style: TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SettingsScreen(),
+                  ),
+                );
+              },
+              child: Text('Go to settings'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => OrdersScreen(),
+                  ),
+                );
+              },
+              child: Text('Go to orders'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProductList(),
+                  ),
+                );
+              },
+              child: Text('Go to Product List'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Settings',
+              style: TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  //Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> HomeScreen()));
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()),
+                      (route) => false);
+                },
+                child: Text('Home'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Orders'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Orders',
+              style: TextStyle(fontSize: 24),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SettingsScreen(),
+                  ),
+                );
+              },
+              child: Text('Go to settings'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                'Back to home',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductList extends StatelessWidget {
+  const ProductList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product List'),
+      ),
+      body: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (context, index) {
+            return ListTile(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) {
+                          return ProductDetailsList(productName: index.toString(),price: 58.65,);
+                        })).then((value) {
+                          print(value);
+                });
+              },
+              title: Text(index.toString()),
+              subtitle: Text('Product Details ${index + 1}'),
+            );
+          }),
+    );
+  }
+}
+
+class ProductDetailsList extends StatelessWidget {
+  final String productName; /// mandatory/required
+  final double? price;  /// optional
+  const ProductDetailsList({super.key, required this.productName , this.price});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Product Details'),
+        ),
+        body: Center(
+          child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                productName,
+                style: TextStyle(fontSize: 24),
+              ),
+              ElevatedButton(onPressed: (){
+                Navigator.pop(context , 'My Name $productName');
+              }, child: Text('Back'))
+            ],
+          ),
+        ));
+  }
+}
+
+ */
