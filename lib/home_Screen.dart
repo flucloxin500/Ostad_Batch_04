18e507f02c08094e69ef40dff07aa46d5826c6de
flucloxin500 <mdwalid826@gmail.com>
2023-10-02 23:31:33 +0600

    -Stack -Align -Positioned -Device preview -Sizer

--- lib/home_Screen.dart
+++ lib/home_Screen.dart
@@ -1,51 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:sizer/sizer.dart';
 
-/// MediaQuery
-
+/// Stack , Positioned , align
+/// Responsive layout builder : resposive builder
+/// Device Preview : device_preview
+/// Size(height,width) : sizer
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
-      ),
-      body: Center(
-        child: Stack(
-          children: [
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.pink,
-            ),
-            Container(
-              width: 50,
-              height: 50,
-              color: Colors.green,
-            ),
-            Positioned.fill(
-              child: Align(
-                alignment: Alignment.center,
-              child: Container(
-                width: 30,
-                height: 30,
-                color: Colors.red,
-              ),
-            ),
-            ),
-            Positioned(
-              top: 0,
-              right: 0,
-              child: Container(
-                width: 15,
-                height: 15,
-                color: Colors.orange,
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Home Screen'),
         ),
-      ),
-    );
+        body: ResponsiveBuilder(
+          builder: (context, sizeInformation) {
+            return Center(
+                child: Text(
+                    sizeInformation.deviceScreenType.toString(),
+                  style: TextStyle(
+                    fontSize: 15.sp
+                  ),
+                ));
+          },
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,47 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
 
 import 'home_Screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      DevicePreview(
+        enabled: kDebugMode,
+          builder: (context) {
+            return MyApp() ;
+          }));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
+  Widget build(BuildContext context)
+  {
+
+    return Sizer(
+      builder: (context , orientation , deviceType) {
+        print(deviceType);
+        return MaterialApp(
+
+          locale: DevicePreview.locale(context),
+          builder: DevicePreview.appBuilder,
+          home: HomeScreen(),
+        );
+      }
     );
   }
-}
+  // {
+  //
+  //           return MaterialApp(
+  //             useInheritedMediaQuery: true,
+  //             locale: DevicePreview.locale(context),
+  //             builder: DevicePreview.appBuilder,
+  //             home: HomeScreen(),
+  //           );
+  //         }
+  }
+
